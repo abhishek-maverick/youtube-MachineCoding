0b53a1c0d9e1616725c33d798c69d3a9245b2bcc
abhishek-maverick <abhishek.ranjan@roado.co.in>
2023-03-01 00:51:35 +0530

    chore: added cleanUp for liveChat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import {
@@ -9,29 +9,58 @@ import {
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
     const i = setInterval(() => {
       //API Polling
-
       dispatch(
         addMessage({
           name: generateRandomName(),
           message: generateRandomMessages(30) + generateRandomEmoji(1),
         })
       );
-    }, 2000);
+    }, 1500);
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className="p-2 border border-black w-full h-[521px] ml-2 bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-      {chatMessages.map((chat, index) => (
-        <ChatMessage key={index} name={chat.name} message={chat.message} />
-      ))}
-    </div>
+    <>
+      <div className="p-2 border border-black w-full h-[521px] ml-2 bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        {chatMessages.map((chat, index) => (
+          <ChatMessage key={index} name={chat.name} message={chat.message} />
+        ))}
+      </div>
+      <form
+        className=" w-full p-2 m-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          className="w-80 border border-black px-2"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button
+          className="px-2 mx-2 bg-green-100"
+          onClick={() => {
+            dispatch(
+              addMessage({
+                name: generateRandomName(),
+                message: liveMessage,
+              })
+            );
+            setLiveMessage("");
+          }}
+        >
+          Send
+        </button>
+      </form>
+    </>
   );
 };
 
